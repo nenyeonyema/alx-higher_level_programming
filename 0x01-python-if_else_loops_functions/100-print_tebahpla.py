43f370c9c340dcb8744a54441caeeaf966334df2
nenyeonyema <chinenyeonyema1@gmail.com>
2023-10-04 17:03:43 -0700

    prints alphabets in reversed order

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-
 for rev_letter in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format(rev_letter if rev_letter % 2 == 0 else rev_letter - 32), end='')
+    if rev_letter % 2 == 0:
+        print("{:c}".format(rev_letter), end='')
+    else:
+        print("{:c}".format(rev_letter - 32), end='')
