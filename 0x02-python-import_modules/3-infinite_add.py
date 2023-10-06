25afa01c350249428ebae2cf7503b59cc385c878
nenyeonyema <chinenyeonyema1@gmail.com>
2023-10-06 02:41:15 -0700

    add command line arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    len_args = len(argv) - 1  # Subtract 1 to exclude the script name itself
+    args = argv[1:]  # Exclude the script name from the list
+
+    if len_args == 0:
+        print(int("0"))
+    result = 0
+    for i, items in enumerate(args, start=1):
+        result = result + int(items)
+    print("{}".format(result))
