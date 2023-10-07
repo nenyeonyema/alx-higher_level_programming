8f2869f2108e1f010ec1fac9297d5daa32699f25
nenyeonyema <chinenyeonyema1@gmail.com>
2023-10-07 16:44:03 -0700

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,8 +5,6 @@ if __name__ == "__main__":
     len_args = len(argv) - 1  # Subtract 1 to exclude the script name itself
     args = argv[1:]  # Exclude the script name from the list
 
-    if len_args == 0:
-        print(int("0"))
     result = 0
     for items in args:
         result = result + int(items)
