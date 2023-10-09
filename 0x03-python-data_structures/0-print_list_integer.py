5aa629479487121653c6afbc11afa9e02e9ebde7
nenyeonyema <chinenyeonyema1@gmail.com>
2023-10-09 07:04:31 -0700

    prints list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,7 @@
 
 
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{}".format(i))
+    if my_list is None:
+        my_list = []
+    for item in range(len(my_list)):
+        print("{:d}".format(my_list[item]))
