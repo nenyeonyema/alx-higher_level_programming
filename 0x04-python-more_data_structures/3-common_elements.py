97c483356cb7b09fad155abc1ab062a0ee471a4e
nenyeonyema <chinenyeonyema1@gmail.com>
2023-10-11 15:04:08 -0700

    common elements in a set

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# a function that returns a set of common elements in two sets.
+
+
+def common_elements(set_1, set_2):
+    common_element = set_1 & set_2
+    return (common_element)
