364053a600fef4c767a2431f0d5837fe86e18af4
nenyeonyema <chinenyeonyema1@gmail.com>
2023-11-11 14:13:47 -0800

    adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" a script that adds all arguments to a Python list, and then save them to a file"""
+"""
+a script that adds all arguments to a
+Python list, and then save them to a file
+"""
 import sys
 
 
