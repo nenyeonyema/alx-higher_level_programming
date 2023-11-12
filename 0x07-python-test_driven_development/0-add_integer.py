5d54407b9728f2abea827554ee59e417150780b8
nenyeonyema <chinenyeonyema1@gmail.com>
2023-11-12 10:27:50 -0800

    doctest of a function that adds two numbers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
+""" a function that adds two integer """
 
 
 def add_integer(a, b=98):
-    """ a function that adds two integer """
+    """
+    a function that adds two integer.
+    Parameters:
+        a (int): expects an int argument.
+        b (int): expects an int arguement.
+    Returns:
+        The addition of a and b, if float arguments
+        are received, they re both typecasted to an int
+        before addition is performed.
+    Raises:
+        TypeError: if none of the arguments is either an int or float.
+    """
 
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     if not (isinstance(b, int) or isinstance(b, float)):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
+
+# print(add_integer.__doc__)
