197c736b88f094cda1e4e362267eb9658e4e441d
nenyeonyema <chinenyeonyema1@gmail.com>
2023-11-13 17:05:50 -0800

    interactive unittest

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__("6-max_integer").max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max_int(self):
+        """ asserts that the max int returned is equal"""
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([1, 3, 4, 2]), 4)
