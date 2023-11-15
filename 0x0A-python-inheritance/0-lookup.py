ea13755e215cea319b2716f708454d0b822daea9
nenyeonyema <chinenyeonyema1@gmail.com>
2023-11-15 08:37:06 -0800

    list attributes and methods used in an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" list of available attributes and methods of an object:"""
+
+
+def lookup(obj):
+    """ Returns:
+    - obj (list): a list of attributes and methods.
+    """
+    return list(dir(obj))
