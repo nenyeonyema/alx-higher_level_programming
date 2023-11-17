6999f1814b689e94666783c618e108bbc6574add
nenyeonyema <chinenyeonyema1.com>
2023-11-17 15:52:32 -0800

    subclass rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A SubClass"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ A rectangle class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Rectangle object constructor"""
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
