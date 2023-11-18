cf1e1885f31dfef327dfc5ed3406beaf4dc77ea6
nenyeonyema <chinenyeonyema1.com>
2023-11-17 16:02:48 -0800

    subclass rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,8 +7,8 @@ class Rectangle(Base):
     """ A rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Rectangle object constructor"""
-        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
+        super().__init__(id)
