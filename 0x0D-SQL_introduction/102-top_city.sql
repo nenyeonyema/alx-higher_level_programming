62dde6a332db38cbb7b428b9725368da66359881
nenyeonyema <chinenyeonyema1@gmail.com>
2023-12-14 04:40:57 -0800

    display max temp

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,8 @@
 --  a script that displays the top 3 of cities temperature
 -- during July and August ordered by temperature (descending)
 SELECT city,
-       MAX(temperature) as max_temperature
-FROM temperature_data
+       MAX(value) as max_temperature
+FROM temperatures
 WHERE MONTH(date) IN (7, 8)
 GROUP BY city
 ORDER BY max_temperature DESC
