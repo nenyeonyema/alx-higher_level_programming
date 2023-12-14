610890e90efa55a7923b451a4f9fe63925c01bf3
nenyeonyema <chinenyeonyema1@gmail.com>
2023-12-14 01:41:15 -0800

    table description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,6 @@
 -- a script that prints the full description of the table
 -- first_table from the database hbtn_0c_0 in your MySQL server.
+USE `hbtn_0c_0`;
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
