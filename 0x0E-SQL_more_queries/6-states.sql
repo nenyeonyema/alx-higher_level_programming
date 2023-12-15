33daf23104c62564d0ddd5882306ffbac6b577fe
nenyeonyema <chinenyeonyema1@gmail.com>
2023-12-15 05:26:42 -0800

    auto increnment

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,6 +2,6 @@
 -- states (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-	id INT PRIMARY KEY NOT NULL UNIQUE,
+	id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL
 );
