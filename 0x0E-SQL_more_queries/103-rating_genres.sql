917fe9db6a9e5fa1e4ca0d76d4342206721440e4
nenyeonyema <chinenyeonyema1@gmail.com>
2023-12-16 05:03:42 -0800

    rating genres

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+--  a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+SELECT tv_genres.name, SUM(tv_ratings.rating) AS rating_sum
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_ratings ON tv_shows.id = tv_ratings.show_id
+GROUP BY tv_genres.name
+ORDER BY rating_sum DESC;
