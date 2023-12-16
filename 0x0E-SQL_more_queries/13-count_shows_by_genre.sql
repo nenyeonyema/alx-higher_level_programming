2d64cac345cb803d9002b5c31327ebae03e75dff
nenyeonyema <chinenyeonyema1@gmail.com>
2023-12-16 03:26:56 -0800

    count number of shows

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- a script that lists all genres from hbtn_0d_tvshows
+-- and displays the number of shows linked to each.
+SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM genres
+JOIN tv_show_genres ON genres.id = tv_show_genres.show_id
+GROUP BY genres.name
+ORDER BY number_of_shows DESC;
