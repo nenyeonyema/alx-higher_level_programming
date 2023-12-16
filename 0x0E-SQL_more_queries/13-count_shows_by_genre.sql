09c544f62be68c8a3c1b4e85e8618193dfd752da
nenyeonyema <chinenyeonyema1@gmail.com>
2023-12-16 03:37:11 -0800

    count number of shows

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,4 @@ SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM genres
 JOIN tv_show_genres ON genres.id = tv_show_genres.show_id
 GROUP BY genres.name
-ORDER BY number_of_shows DESC;
+ORDER BY number_of_shows DESC, genres.id ASC;
