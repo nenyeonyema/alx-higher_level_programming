06ca1e89e5ee60bb9cdd1e12d61ff84abb1d1187
nenyeonyema <chinenyeonyema1@gmail.com>
2023-12-16 04:12:20 -0800

    count num of genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,6 @@
 -- a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
-
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre.id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_genres.name ASC;
