0ed2dcc147d6f81d2577b2e712f1a12dcddfb66a
nenyeonyema <chinenyeonyema1@gmail.com>
2024-01-09 13:08:29 -0800

    second script

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const firstline = 'C is fun';
+const secondline = 'Python is cool';
+const lastline = 'JavaScript is amazing';
+console.log(firstline);
+console.log(secondline);
+console.log(lastline);
