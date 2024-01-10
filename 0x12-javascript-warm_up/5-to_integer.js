de6fdd95f499acccaee4d0ac322d42ecc0a4b8a4
nenyeonyema <chinenyeonyema1@gmail.com>
2024-01-10 03:12:20 -0800

    modified

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const num = Number(process.argv[2]);
+const num = parseInt(process.argv[2]);
 if (!isNaN(num)) {
-  console.log(`My Number: ${num}`);
+  console.log(`My number: ${num}`);
 } else {
   console.log('Not a number');
 }
