231dc18955e0c2946c101fa86f5aad70c985477f
nenyeonyema <chinenyeonyema1@gmail.com>
2024-01-10 05:53:35 -0800

    a function that adds two integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function add(a, b) {
+  return (a + b);
+}
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+if (a && b) {
+  console.log(add(a, b));
+} else {
+  console.log('NaN');
+}
