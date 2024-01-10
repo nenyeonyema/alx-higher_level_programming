79d9611f8aa9457c092d059b814b894cb7eabb51
nenyeonyema <chinenyeonyema1@gmail.com>
2024-01-10 04:13:26 -0800

    looped from argument

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2]);
+if (!isNaN(num) && num > 0) {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+} else if (!isNaN(num) && num <= 0) {
+} else {
+  console.log('Missing number of occurrences');
+}
