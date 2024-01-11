0d9fb0de2311720f769bebacbe5a528e4ddb7c0d
nenyeonyema <chinenyeonyema1@gmail.com>
2024-01-11 13:49:53 -0800

    number of occurences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (let element of list) {
+  for (const element of list) {
     if (element === searchElement) {
       count += 1;
     }
