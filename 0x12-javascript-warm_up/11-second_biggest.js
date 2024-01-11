8e4dc1d408ba1eead8ee2291f0318c0abce6f6db
nenyeonyema <chinenyeonyema1@gmail.com>
2024-01-11 02:35:53 -0800

    second largest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,16 +6,16 @@ function secondbig (args) {
     return (0);
   }
   for (let i = 0; i < args.length - 1; i++) {
-    for (let j = 0; j  < args.length - i - 1; j++) {
+    for (let j = 0; j < args.length - i - 1; j++) {
       let templarge = 0;
       if (args[j] < args[j + 1]) {
         templarge = args[j + 1];
-	args[j + 1] = args[j];
-	args[j] = templarge;
+        args[j + 1] = args[j];
+        args[j] = templarge;
       }
     }
   }
-  return (args[1])
+  return (args[1]);
 }
 const numsArray = process.argv.slice(2).map(Number);
 console.log(secondbig(numsArray));
