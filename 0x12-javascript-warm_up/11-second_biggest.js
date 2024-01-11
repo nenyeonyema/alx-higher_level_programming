b2ea237be3839e0cf5fad667e9ff97ee1b91c768
nenyeonyema <chinenyeonyema1@gmail.com>
2024-01-11 02:33:32 -0800

    second largest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// An array of integer arguments being passed
+// to the function to return the second biggest argument.
+function secondbig (args) {
+  if (args.length <= 1) {
+    return (0);
+  }
+  for (let i = 0; i < args.length - 1; i++) {
+    for (let j = 0; j  < args.length - i - 1; j++) {
+      let templarge = 0;
+      if (args[j] < args[j + 1]) {
+        templarge = args[j + 1];
+	args[j + 1] = args[j];
+	args[j] = templarge;
+      }
+    }
+  }
+  return (args[1])
+}
+const numsArray = process.argv.slice(2).map(Number);
+console.log(secondbig(numsArray));
