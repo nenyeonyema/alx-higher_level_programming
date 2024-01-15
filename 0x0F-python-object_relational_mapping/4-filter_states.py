f9508c4f0f452fbb48ec18be01c58a41e05b271e
nenyeonyema <chinenyeonyema1.com>
2024-01-15 13:31:50 -0800

    filter states

--- 0x0F-python-object_relational_mapping/4-filter_states.py
+++ 0x0F-python-object_relational_mapping/4-filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""A script that lists all states from the database hbtn_0e_0_usa"""
+"""A script that lists states from the database hbtn_0e_0_usa"""
 import sys
 import MySQLdb
 
 
-def filter_states(username, password, database):
-    """Connects to MySQL database and lists states starting with 'N'"""
+def select_states(username, password, database):
+    """Connects to MySQL database and selects states starting with 'N'"""
 
     try:
         # Connect to MySQL database
@@ -20,15 +20,19 @@ def filter_states(username, password, database):
         cursor = db.cursor()
 
         # Execute SQL query to select states starting with 'N'
-        cursor.execute("SELECT * FROM states\
-                WHERE name LIKE 'N%' ORDER BY id ASC")
+        cursor.execute("SELECT * FROM states WHERE \
+                name LIKE 'N%' ORDER BY id ASC")
 
         # Fetch all rows from the result set
         rows = cursor.fetchall()
 
-        # Display results
+        # Display results without duplicates
+        seen_states = set()
         for row in rows:
-            print(row)
+            state_id, state_name = row
+            if state_name not in seen_states:
+                seen_states.add(state_name)
+                print(row)
 
     except MySQLdb.Error as e:
         print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
@@ -44,14 +48,13 @@ def filter_states(username, password, database):
 if __name__ == "__main__":
     # Check if three arguments are provided
     if len(sys.argv) != 4:
-        print("Usage: ./1-filter_states.py\
-                <username> <password> <database_name>")
+        print("Usage: ./1-filter_states.py <username> <password> <database>")
         sys.exit(1)
 
     # Get command-line arguments
     username = sys.argv[1]
     password = sys.argv[2]
-    database_name = sys.argv[3]
+    database = sys.argv[3]
 
-    # List and display states starting with 'N'
-    filter_states(username, password, database_name)
+    # Select and display states starting with 'N'
+    select_states(username, password, database)
