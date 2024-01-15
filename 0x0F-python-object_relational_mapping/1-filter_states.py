ce9b099e48dd5994028e45641630e3e3fd8564a9
nenyeonyema <chinenyeonyema1.com>
2024-01-15 03:34:34 -0800

    modified

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-""" a script that lists all states from the database hbtn_0e_0_usa """
+""" Lists states with N names"""
 import sys
 import MySQLdb
 
 
-def filter_list(username, password, database):
-    """ List all states with a name starting with N """
+if __name__ == "__maini__":
     try:
+        # Check if correct number of arguments is provided
+        if len(sys.argv) != 4:
+            raise ValueError("Usage: {} \
+                    <username> <password> <database>".format(sys.argv[0]))
+
+        # Extract command line arguments
+        username, password, database = sys.argv[1:]
+
         # Connect to MySQL server
         db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database,
+            charset="utf8"
         )
 
         # Create a cursor object to interact with the database
@@ -30,19 +38,16 @@ def filter_list(username, password, database):
         for row in rows:
             print(row)
 
-        # Close cursor and database connection
-        cursor.close()
-        db.closei()
-
     except MySQLdb.Error as e:
-        print("Error connecting to MySQL: ", e)
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
         sys.exit(1)
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    except Exception as e:
+        print("Error: {}".format(e))
         sys.exit(1)
 
-    username, password, database = sys.argv[1:]
-
-    filter_list(username, password, database)
+    finally:
+        # Close cursor and database connection in the finally block
+        if 'db' in locals() and db:
+            cursor.close()
+            db.close()
---
+++ 0x0F-python-object_relational_mapping/2-filter_states.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" a script that lists all states from the database hbtn_0e_0_usa """
+import sys
+import MySQLdb
+
+
+def filter_list(username, password, database):
+    """ List all states with a name starting with N """
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+        )
+
+        # Create a cursor object to interact with the database
+        cursor = db.cursor()
+
+        # Execute the SQL query to retrieve states starting with 'N'
+        query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+        cursor.execute(query)
+
+        # Fetch all the rows
+        rows = cursor.fetchall()
+
+        # Display the results
+        for row in rows:
+            print(row)
+
+        # Close cursor and database connection
+        cursor.close()
+        db.closei()
+
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL: ", e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    username, password, database = sys.argv[1:]
+
+    filter_list(username, password, database)
