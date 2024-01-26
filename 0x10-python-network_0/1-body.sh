b115a99165dcdde432b838260cce7d89d4bb027e
nenyeonyema <chinenyeonyema1@gmail.com>
2024-01-26 13:23:59 -0800

    get modified

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to the provided URL using curl
-curl -s -X GET -w "%{http_code}" "$1" -o temp.txt && [[ $(cat temp.txt) == "200" ]] && cat temp.txt | tail -n +2
+curl -s -o /tmp/response -w "%{http_code}" "$1" | { read code; if [ $code -eq 200 ]; then cat /tmp/response; fi; }
