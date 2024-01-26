73a607ff9cba62ae3408ac9a67f805e584fa6645
nenyeonyema <chinenyeonyema1@gmail.com>
2024-01-26 13:46:36 -0800

    request

---
+++ 0x10-python-network_0/100-status_code.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script sends a request to the specified URL
+response=$(mktemp)
+
+curl -s -o "$response" -w "%{http_code}" "$1"
+
+status_code=$(cat "$response")
+echo "$status_code"
+
+rm "$response"
