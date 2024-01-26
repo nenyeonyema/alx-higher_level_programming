75fc1e4c75c11bcf7880e6218ea6080a61fda761
nenyeonyema <chinenyeonyema1@gmail.com>
2024-01-26 11:43:18 -0800

    modified

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-# Check if the script is provided with the correct number of arguments
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a request to the URL using curl and store the response body in a temporary file
-response_body=$(mktemp)
-curl -s "$1" -o "$response_body"
-
-# Calculate the size of the response body in bytes
-body_size=$(stat -c %s "$response_body")
-
-# Display the size of the response body
-echo "$body_size"
-
-# Clean up the temporary file
-rm "$response_body"
+# This script takes a URL as input, sends a request using curl
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
